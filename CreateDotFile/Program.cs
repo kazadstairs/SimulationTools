710d12460deca9de50e17772a7ffb39c16949bf2
3496724 <mshessey@gmail.com>
2018-08-15 16:28:51 +0200

    CreateDotFile build ready. Do not modify.

--- CreateDotFile/Program.cs
+++ CreateDotFile/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace CreateDotFile
 {
+    //cmd commands: CreateDotFile.exe < %1 >temp.dotin &&  dot -Tpng <temp.dotin >%2 && %2. 
+    //Make sure dot is in the PATH enviroment( use SET PATH=%PATH%;<path to bin folder containing dot>)
     class Program
     {
         static void Main(string[] args)
@@ -14,22 +16,21 @@ namespace CreateDotFile
 
             int Machines = int.Parse(Console.ReadLine());
             int Njobs = int.Parse(Console.ReadLine());
-            string[] J = new string[Njobs];
+            string[] J = new string[Njobs+1];
             string[] jobs = Console.ReadLine().Split();
-            for (int jobID = 0; jobID < Njobs; jobID++)
+            for (int jobID = 1; jobID < Njobs+1; jobID++)
             {
-                J[jobID] = string.Format("Id:{0}-info:{1}", jobID, jobs[jobID]); // replace with better name if wanted
+                J[jobID] = string.Format("\"{0}|{1}\"", jobID,jobs[jobID]); // replace with better name if wanted , jobs[jobID]
             }
 
 
             string[] splitline;
-            for (int rowid = 0; rowid < Njobs; rowid++)
+            for (int rowid = 1; rowid < Njobs+1; rowid++)
             {
                 splitline = Console.ReadLine().Split();
-                for (int colid = 0; colid < Njobs; colid++)
+                for (int colid = 1; colid < Njobs+1; colid++)
                 {
-                    if (splitline[colid] == "") break; // empty thing
-                    else if (splitline[colid] != "-")
+                    if (splitline[colid] != "-")
                     {
                         Console.WriteLine("{0} -> {1}", J[rowid], J[colid]);
                     }
